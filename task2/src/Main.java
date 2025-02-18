5e52ca2d01a8530f5e776da26c9baad6d832e109
SabyrrZhaslanuly <sabyrr.zhaslanuly@gmail.com>
2025-02-18 19:52:41 +0500

    The sixth homework has been done

--- task2/src/Main.java
+++ task2/src/Main.java
@@ -13,22 +13,41 @@ public class Main {
         System.out.println("Введите третий вес: ");
         double third = new Scanner(System.in).nextDouble();
 
-        double greatest = 0;
-        double average = 0;
-        double smallest = 0;
+        double greatest;
+        double average;
+        double smallest;
         //todo дописать логику программы ниже.
 
+        /*if (first >= second && first >= third) {
+            greatest = first;
+        } else if (second >= first && second >= third) {
+            greatest = second;
+        } else {
+            greatest = third;
+        }*/
+        greatest = (first >= second && first >= third) ? first :
+                (second >= first && second >= third) ? second : third;
 
+        /*if (first <= second && first <= third) {
+            smallest = first;
+        } else if (second <= first && second <= third) {
+            smallest = second;
+        } else {
+            smallest = third;
+        }*/
 
+        smallest = (first <= second && first <= third) ? first :
+                (second <= first && second <= third) ? second : third;
 
+        double sumOfNumbers = first + second + third;
 
-
+        average = sumOfNumbers - greatest - smallest;
 
         //todo
 
 
-        System.out.println("Наибольший вес: " + 10);
-        System.out.println("Средний вес: " + 9);
-        System.out.println("Наименьший вес: " + 1);
+        System.out.println("Наибольший вес: " + greatest);
+        System.out.println("Средний вес: " + average);
+        System.out.println("Наименьший вес: " + smallest);
     }
 }
